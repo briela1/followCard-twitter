1521f7c33fe9ca278fdfd0ef49821a8d1ee44ee9
briela1 <98447320+briela1@users.noreply.github.com>
2023-02-20 11:51:45 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export function App() {
+
+
+  return (
+    <div>
+    <h1>Twitter Card</h1> 
+    </div>
+  )
+}
+
+
